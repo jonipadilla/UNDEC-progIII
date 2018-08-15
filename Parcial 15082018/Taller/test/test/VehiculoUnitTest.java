cab3afa52ba8bf69a5660d00e5739e57390fa2cb
Javier Ruitti <ruittijavier@gmail.com>
2018-08-15 15:29:57 -0300

    Parcial 15082018

---
+++ Parcial 15082018/Taller/test/test/VehiculoUnitTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import clases.Vehiculo;
+
+class VehiculoUnitTest {
+
+	//Atributos Vehiculo: ID, Marca y Modelo, Patente, Año
+
+	@Test
+	void instanciarVehiculo_VehiculoSinPatente_generaError() {
+		
+		Vehiculo elVehiculo=Vehiculo.instancia(1, "VW Golf", null, 2009);
+		
+		assertNull(elVehiculo);		
+	}
+	
+	@Test
+	void instanciarVehiculo_VehiculoCompleto_instanciaCorrecta() {
+		Vehiculo elVehiculo=Vehiculo.instancia(1, "VW Golf", "ABC123", 2009);
+		
+		assertNotNull(elVehiculo);		
+	}
+	
+	@Test
+	void mostrarVehiculo_formatoEspecifico_muestraFormateado() {
+		Vehiculo elVehiculo=Vehiculo.instancia(1,"VW Golf","ABC123",2009);
+		String formatoSalida=elVehiculo.mostrarVehiculo();
+		
+		assertEquals("VW Golf modelo 2009 - Patente ABC123", formatoSalida);
+	}
+
+}
